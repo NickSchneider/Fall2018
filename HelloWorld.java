e63d14044e89cf78b2c76f2a8e6f4ead9f494685
Nick <nickschneider54@gmail.com>
2018-10-29 16:23:50 -0500

    Added a line about Fall

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,5 @@ public void main(String[] args)
 {
 	System.out.println("Hello World");
 	System.out.println("My name is Nick");
+	System.out.println("The changing colors of the leaves makes Fall more enjoyable");
 }
