c952aa046a206eb3c1dba0376fffe03c828781d5
Nick <nickschneider54@gmail.com>
2018-10-29 16:27:59 -0500

    Added a for loop to count 10 leaves falling

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,9 @@ public void main(String[] args)
 	System.out.println("Hello World");
 	System.out.println("My name is Nick");
 	System.out.println("The changing colors of the leaves makes Fall more enjoyable");
+	for(int i = 1; i <= 10; i++)
+	{
+		System.out.println(i + " Leaves have fallen");
+	}
+
 }
